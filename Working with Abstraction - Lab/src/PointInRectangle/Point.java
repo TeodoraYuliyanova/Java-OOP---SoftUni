425a2d14b45eb2b32abeb4a6f0a02797ed279e34
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2022-11-09 23:37:17 +0000

    Add files via upload

---
+++ Working with Abstraction - Lab/src/PointInRectangle/Point.java
@@ -0,0 +1,20 @@
+package PointInRectangle;
+
+public class Point {
+
+    private int xCoordinate;
+    private int yCoordinate;
+
+    public Point(int xCoordinate, int yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
+}
