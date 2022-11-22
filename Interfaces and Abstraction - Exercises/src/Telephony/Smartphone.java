82dab22175500066a626727618ec6932e34498d3
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2022-11-22 23:54:16 +0000

    Add files via upload

---
+++ Interfaces and Abstraction - Exercises/src/Telephony/Smartphone.java
@@ -0,0 +1,61 @@
+package Telephony;
+
+import java.util.List;
+
+public class Smartphone implements Browsable, Callable {
+
+    private List<String> numbers;
+    private List<String> urls;
+
+    public Smartphone(List<String> numbers, List<String> urls) {
+        this.numbers = numbers;
+        this.urls = urls;
+    }
+
+
+    @Override
+    public String browse() {
+        StringBuilder sb = new StringBuilder();
+
+        for (String url : urls) {
+            if (hasNumbers(url)) {
+                sb.append("Invalid URL!").append(System.lineSeparator());
+            } else {
+                sb.append(String.format("Browsing: %s!", url)).append(System.lineSeparator());
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    @Override
+    public String call() {
+        StringBuilder sb = new StringBuilder();
+
+        for (String number : numbers) {
+            if (hasOnlyNumbers(number)) {
+                sb.append(String.format("Calling... %s", number)).append(System.lineSeparator());
+            } else {
+                sb.append("Invalid number!").append(System.lineSeparator());
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    private boolean hasOnlyNumbers(String text) {
+        for (int i = 0; i < text.length(); i++) {
+            if (!Character.isDigit(text.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean hasNumbers(String text) {
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isDigit(text.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
