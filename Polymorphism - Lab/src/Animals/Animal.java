e0082371090cbe259da7bfca83b783194c4267f5
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2022-11-23 22:53:45 +0000

    Add files via upload

---
+++ Polymorphism - Lab/src/Animals/Animal.java
@@ -0,0 +1,22 @@
+package Animals;
+
+public abstract class Animal {
+
+    private String name;
+    private String favouriteFood;
+
+    public Animal(String name, String favouriteFood) {
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public abstract String explainSelf();
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+}
---
+++ Polymorphism - Lab/src/Animals/Dog.java
@@ -0,0 +1,15 @@
+package Animals;
+
+public class Dog extends Animal{
+
+
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s%n" +
+                "DJAAF" ,getName(), getFavouriteFood());
+    }
+}
